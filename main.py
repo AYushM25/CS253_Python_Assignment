434a8b7e6a8d3cf472a2fce834d360a35596d15a
Ayush Meena <121339676+AYushM25@users.noreply.github.com>
2024-04-14 00:05:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from sklearn.model_selection import GridSearchCV
 from sklearn.naive_bayes import BernoulliNB
-from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-import numpy as np
 import pandas as pd
 
 train_data = pd.read_csv('train.csv')
